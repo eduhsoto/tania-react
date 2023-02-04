a009020c5cbf8b711acc9a397b57e24bdc4f30d4
Eduardo Hernández Soto <hseduardo@gmail.com>
2023-02-04 17:00:10 -0600

    login auth and fix error eslint rules confilct

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,16 +15,28 @@ module.exports = {
     'react-app',
     'react-app/jest',
     'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['tsconfig.json'],
+    tsconfigRootDir: __dirname,
   },
   plugins: ['react', 'react-hooks'],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        "checksVoidReturn": false
+      }
+    ]
   },
+  ignorePatterns: [
+    ".eslintrc.js"
+],
 };
