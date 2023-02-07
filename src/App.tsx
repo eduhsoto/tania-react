41d69cf6d07e6f41244a41a1b2eab5e35b41bdab
Eduardo Hernández Soto <hseduardo@gmail.com>
2023-02-06 21:02:40 -0600

    context auth and and implements login from firebase

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,25 @@ import Portafolio from './pages/Portafolio'
 import Footer from './components/Footer'
 import './assets/index.css'
 import Login from './pages/Login'
+import Dashboard from './pages/Dashboard/Dashboard'
+import { AuthProvider } from './auth/authContext'
 
 const App = (): JSX.Element => {
   return (
     <>
       <NavBar />
+      <AuthProvider>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/my-path' element={<Path />} />
         <Route path='/portafolio' element={<Portafolio />} />
         <Route path='/login' element={<Login />} />
         <Route path='*' element={<Navigate to='/' />} />
+        
+        <Route path='/dashboard' element={<Dashboard />}/>
+        
       </Routes>
+      </AuthProvider>
       <Footer />
     </>
   )
