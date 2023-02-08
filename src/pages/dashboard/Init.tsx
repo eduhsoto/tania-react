845880d45fafcac7e5a2905f2bfb3709ceacceea
Eduardo Hernández Soto <hseduardo@gmail.com>
2023-02-07 19:44:37 -0600

    boolean render is user exists

---
+++ src/pages/dashboard/Init.tsx
@@ -0,0 +1,15 @@
+const Init = (): JSX.Element => {
+  return (
+      <div>
+        <h1>Hola Tania</h1>
+        <div>
+
+        </div>
+        <button>Cerrar sesión</button>
+      </div>
+
+  )
+}
+
+
+export default Init
